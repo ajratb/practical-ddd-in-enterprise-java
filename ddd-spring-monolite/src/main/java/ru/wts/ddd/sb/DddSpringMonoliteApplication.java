eeb5ece9d2db980270348220857d1f3fe56602c7
Ayrat Bik <ajratbik@gmail.com>
2021-06-15 10:21:25 +0700

    add project: ddd-sb-monolite

---
+++ ddd-spring-monolite/src/main/java/ru/wts/ddd/sb/DddSpringMonoliteApplication.java
@@ -0,0 +1,13 @@
+package ru.wts.ddd.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddSpringMonoliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddSpringMonoliteApplication.class, args);
+	}
+
+}
