cf4233c1b158aa211157a1ad844843cbce32afd5
Ayrat Bik <ajratbik@gmail.com>
2021-06-16 10:47:58 +0700

    add booking classes and start to remake into spring boot app

---
+++ ddd-spring-monolite/src/main/java/com/practicalddd/cargotracker/booking/application/internal/queryservices/CargoBookingQueryService.java
@@ -0,0 +1,47 @@
+package com.practicalddd.cargotracker.booking.application.internal.queryservices;
+
+
+import com.practicalddd.cargotracker.booking.domain.model.aggregates.BookingId;
+import com.practicalddd.cargotracker.booking.domain.model.aggregates.Cargo;
+import com.practicalddd.cargotracker.booking.infrastructure.repositories.jpa.CargoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Application Service which caters to all queries related to the Booking Bounded Context
+ */
+@Service
+public class CargoBookingQueryService {
+
+
+    @Autowired
+    private CargoRepository cargoRepository; // Inject Dependencies
+
+    /**
+     * Find all Cargos
+     * @return List<Cargo>
+     */
+    @Autowired
+    public List<Cargo> findAll(){
+        return cargoRepository.findAll();
+    }
+
+    /**
+     * List All Booking Identifiers
+     * @return List<BookingId>
+     */
+   public List<BookingId> getAllBookingIds(){
+       return cargoRepository.findAllBookingIds();
+   }
+
+    /**
+     * Find a specific Cargo based on its Booking Id
+     * @param bookingId
+     * @return Cargo
+     */
+    public Cargo find(String bookingId){
+        return cargoRepository.find(new BookingId(bookingId));
+    }
+}
--- ddd-spring-monolite/src/test/java/com/practicalddd/cargotracker/DddSpringMonoliteApplicationTests.java
+++ ddd-spring-monolite/src/test/java/com/practicalddd/cargotracker/DddSpringMonoliteApplicationTests.java
@@ -1,4 +1,4 @@
-package ru.wts.ddd.sb;
+package com.practicalddd.cargotracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
